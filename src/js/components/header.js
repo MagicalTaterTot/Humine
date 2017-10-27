f1dd18733003647a4050183be42baef3fcf290fd
Jason Liu <shadowbox826@gmail.com>
2017-10-27 13:35:19 -0400

    Header.js
    Need to add button css

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -5,21 +5,38 @@ class Header extends React.Component {
   render() {
     // Input Elements Here:
     // Types: Text, Dropdown
+    // If dropdown, link -> links, add array of values, otherwise, values is empty array
+    // Use <Link/> to connect to routes
     var elements = {
       1: {
-        name: "About Us",
+        name: "Who are we?",
         type: "Text",
-        link: "/about"
+        values: [],
+        links: ["/who"]
       }
       2: {
-        name: "",
-        type, "",
-        link, ""
+        name: "About Us",
+        type, "Dropdown",
+        values, ["What are we?","Why?","How?"],
+        links, ["/what", "/why", "/how"]
       }
     }
-
-
-  }
+    var buttons = [] 
+    
+    for(i in elements) {
+      let name = i.name;
+      let type = i.type;
+      let values = i.values;
+      let links = i.links;
+      
+      buttons.push(<NavElement 
+        content = { name }
+        type = { type }
+        values = { values }
+        links = { links }
+      />)
+    };
+  };
 }
 
 class NavElement extends React.Component {
@@ -28,7 +45,20 @@ class NavElement extends React.Component {
     this.state = {
       content: this.props.content,
       type: this.props.type,
-      link: this.props.link
+      values: this.props.value,
+      links: this.props.links
     }
   }
+ // Use image opacity to make buttons transparent and change color
+ render() {
+   const type = this.state.type
+   if(type.toUpperCase() === "TEST") {
+     return (
+       <div>
+       // Put button text and style here:
+       </div>
+     )
+   }
+   
+ }
 }
