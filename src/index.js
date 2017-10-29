be56bb42df09dbb3ab1dd0fb96030f61c1251ff2
Danieldylan1 <shadowbox826@gmail.com>
2017-10-28 22:00:03 -0400

    work on dropdowns

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './js/components/header.js'
 
 ReactDOM.render(
   //<Route path = "/about" component = />,
-  <p>Hi!</p>
+  <Header/>,
   document.getElementById('container')
 )
--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,43 +1,50 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-class Header extends React.Component {
+export default class Header extends React.Component {
   render() {
     // Input Elements Here:
     // Types: Text, Dropdown
     // If dropdown, link -> links, add array of values, otherwise, values is empty array
     // Use <Link/> to connect to routes
     var elements = {
-      1: {
+      "1": {
         name: "Who are we?",
         type: "Text",
         values: [],
         links: ["/who"]
-      }
-      2: {
+      },
+      "2": {
         name: "About Us",
-        type, "Dropdown",
-        values, ["What are we?","Why?","How?"],
-        links, ["/what", "/why", "/how"]
+        type: "Dropdown",
+        values: ["What are we?","Why?","How?"],
+        links: ["/what", "/why", "/how"]
       }
     }
-    var buttons = [] 
-    
-    for(i in elements) {
-      let name = i.name;
-      let type = i.type;
-      let values = i.values;
-      let links = i.links;
-      
-      buttons.push(<NavElement 
+    var buttons = []
+    for (var i = 0; i < Object.keys(elements).length; i++) {
+      var buttonInfo = elements[(i + 1).toString()]
+      var name = buttonInfo["name"]
+      var type = buttonInfo["type"]
+      var values = buttonInfo["values"]
+      var links = buttonInfo["links"]
+
+      buttons.push(
+      <NavElement
         content = { name }
         type = { type }
         values = { values }
         links = { links }
-      />)
+      />);
     };
+    console.log(buttons.length)
+    return (
+      <div className="headerContainer">
+        { buttons }
+      </div>
+    );
   };
-}
+};
 
 class NavElement extends React.Component {
   constructor(props) {
@@ -52,13 +59,33 @@ class NavElement extends React.Component {
  // Use image opacity to make buttons transparent and change color
  render() {
    const type = this.state.type
-   if(type.toUpperCase() === "TEST") {
+   if(type.toUpperCase() === "TEXT") {
      return (
        <div>
-       // Put button text and style here:
+        <p>
+          { this.state.content }
+        </p>
        </div>
      )
    }
-   
+   // Add in the rest of the link stuff
+  if(type.toUpperCase() === "DROPDOWN") {
+    var dropdownLinks = []
+    for (var value in values) {
+      dropdownLinks.push(
+        <Link className="dropdown-item">{ value }</Link>
+      );
+    };
+
+    return (
+      <div className="btn-group">
+      <button className="btn dropdown-toggle"></button>
+        <div className="dropdown-menu">
+          { dropdownLinks }
+        </div>
+      </div>
+    )
+  }
+
  }
 }
